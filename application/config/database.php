675d30ce5fe091dcf69626fb80705e9c4c2f8ce2
Lezhni <leznii@gmail.com>
2013-01-24 20:36:10 +0300

    With TWIG and PDO

--- application/config/database.php
+++ application/config/database.php
@@ -46,15 +46,15 @@
 */
 
 $active_group = 'default';
-$active_record = TRUE;
+$active_record = FALSE;;
 
-$db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
+$db['default']['hostname'] = 'mysql:host=localhost;port=3306;dbname=DataBase';
+$db['default']['username'] = 'root';
 $db['default']['password'] = '';
-$db['default']['database'] = '';
-$db['default']['dbdriver'] = 'mysql';
+$db['default']['database'] = 'database';
+$db['default']['dbdriver'] = 'pdo';
 $db['default']['dbprefix'] = '';
-$db['default']['pconnect'] = TRUE;
+$db['default']['pconnect'] = FALSE;
 $db['default']['db_debug'] = TRUE;
 $db['default']['cache_on'] = FALSE;
 $db['default']['cachedir'] = '';
--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -1,6 +1,10 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class Welcome extends CI_Controller {
+	function __construct() {
+		parent::__construct();
+		$this->load->library('twig');
+	}
 
 	/**
 	 * Index Page for this controller.
@@ -19,7 +23,9 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		//$this->load->view('welcome_message');
+		$data['hello'] = 'aaa';
+		echo $this->twig->render('main.twig', $data);
 	}
 }
 
